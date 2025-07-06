70f4488ab14b5b7a6b754e8060261ca1e458626e
JulioMtzSan <julio.climate@gmail.com>
2025-07-06 12:38:16 -0600

    Agregar WRF

---
+++ sietedias/logo2.sh
@@ -0,0 +1,4 @@
+convert Pron_7d_Bayer-S1.jpg logo2.jpeg -geometry +2250+250 -composite Pron_7d_Bayer-S1-2.jpg
+convert TMAX_7d_Bayer-S1.jpg logo2.jpeg -geometry +2250+250 -composite TMAX_7d_Bayer-S1-2.jpg
+convert TMED_7d_Bayer-S1.jpg logo2.jpeg -geometry +2250+250 -composite TMED_7d_Bayer-S1-2.jpg
+convert TMIN_7d_Bayer-S1.jpg logo2.jpeg -geometry +2250+250 -composite TMIN_7d_Bayer-S1-2.jpg
